fb9c963f2bf979569d3b33e717fcf3660c4b656f
James Stow <james.stow@codewizards.co.uk>
2025-06-11 14:42:57 +0100

    adding in last-chance catch to tailscale bin locator to use  to try find the ts binary

--- scripts/shared.sh
+++ scripts/shared.sh
@@ -1,65 +1,69 @@
 #!/usr/bin/env bash
 
-
 get_tmux_option() {
-	local option=$1
-	local default_value=$2
-	local option_value=$(tmux show-option -gqv "$option")
-	if [ -z "$option_value" ]; then
-		echo "$default_value"
-	else
-		echo "$option_value"
-	fi
+  local option=$1
+  local default_value=$2
+  local option_value=$(tmux show-option -gqv "$option")
+  if [ -z "$option_value" ]; then
+    echo "$default_value"
+  else
+    echo "$option_value"
+  fi
 }
 
 set_tmux_option() {
-	local option="$1"
-	local value="$2"
-	tmux set-option -gq "$option" "$value"
+  local option="$1"
+  local value="$2"
+  tmux set-option -gq "$option" "$value"
 }
 
-tailscale_bin(){
-    for op in "$@"; do
-        if command -v "$op" > /dev/null 2>&1; then
-            echo "$op"
-            return 0
-        fi
-    done
-    return 1
-}
-
-current_status(){
-    local ts=$(tailscale_bin /usr/local/bin/tailscale $HOME/go/bin/tailscale /Applications/Tailscale.app/Contents/MacOS/Tailscale)
-    "$ts" status --json | jq -r 'if .BackendState == "Running" then if .Self.Online then "Online" else "Offline" end else .BackendState end'
-}
-
-current_tailnet(){
-    local ts=$(tailscale_bin /usr/local/bin/tailscale $HOME/go/bin/tailscale /Applications/Tailscale.app/Contents/MacOS/Tailscale)
-    "$ts" status --json | jq -r '.CurrentTailnet.Name'
-}
-
-print_icon(){
-    local status=""
-    if [ "$#" == 0 ]; then
-        status=$(current_status)
-    else
-        status=$1
+tailscale_bin() {
+  for op in "$@"; do
+    if command -v "$op" >/dev/null 2>&1; then
+      echo "$op"
+      return 0
     fi
-    if [[ $status == "Online" ]]; then
-        icon="$(get_tmux_option @tailscale_online_icon ✅)"
-    elif [[ $status == "Offline" ]]; then
-        icon="$(get_tmux_option @tailscale_offline_icon ⛔️)"
-    elif [[ $status == "Stopped" ]]; then
-        icon="$(get_tmux_option @tailscale_stopped_icon 🛑)"
-    elif [[ $status == "Starting" ]]; then
-        icon="$(get_tmux_option @tailscale_starting_icon 🔄)"
-    else
-        icon="$(get_tmux_option @tailscale_unknown_icon ❓)"
-    fi
-    echo "$icon"
+  done
+  loc=$(which tailscale)
+  if [ $? -eq 0 ]; then
+    echo $loc
+    return 0
+  fi
+  return 1
 }
 
-print_status(){
-    local status=$(current_status)
-    local icon=$(print_icon $status)
+current_status() {
+  local ts=$(tailscale_bin /usr/local/bin/tailscale $HOME/go/bin/tailscale /Applications/Tailscale.app/Contents/MacOS/Tailscale)
+  "$ts" status --json | jq -r 'if .BackendState == "Running" then if .Self.Online then "Online" else "Offline" end else .BackendState end'
+}
+
+current_tailnet() {
+  local ts=$(tailscale_bin /usr/local/bin/tailscale $HOME/go/bin/tailscale /Applications/Tailscale.app/Contents/MacOS/Tailscale)
+  "$ts" status --json | jq -r '.CurrentTailnet.Name'
+}
+
+print_icon() {
+  local status=""
+  if [ "$#" == 0 ]; then
+    status=$(current_status)
+  else
+    status=$1
+  fi
+  if [[ $status == "Online" ]]; then
+    icon="$(get_tmux_option @tailscale_online_icon ✅)"
+  elif [[ $status == "Offline" ]]; then
+    icon="$(get_tmux_option @tailscale_offline_icon ⛔️)"
+  elif [[ $status == "Stopped" ]]; then
+    icon="$(get_tmux_option @tailscale_stopped_icon 🛑)"
+  elif [[ $status == "Starting" ]]; then
+    icon="$(get_tmux_option @tailscale_starting_icon 🔄)"
+  else
+    icon="$(get_tmux_option @tailscale_unknown_icon ❓)"
+  fi
+  echo "$icon"
+}
+
+print_status() {
+  local status=$(current_status)
+  local icon=$(print_icon $status)
 }
